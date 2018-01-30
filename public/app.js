ee2b36403dd3d4bd97aaf5c6e4efbeccf01cf1b7
draconis79 <mandy.tong.79@gmail.com>
2018-01-29 17:41:54 -0800

    updated routes

--- public/app.js
+++ public/app.js
@@ -4,14 +4,18 @@ const app = angular.module('EverythingListApp', []);
 
 app.controller('MainController', ['$http', function($http) {
 
-  this.list.categories = [];
-  this.list.items = [];
+  this.list_categories = [];
+  this.list_category = {};
+  this.list_item = [];
+  this.list_items = {};
   this.users = [];
+  this.user = {};
   this.url = 'http://localhost:3000/list_categories';
   this.userPass = {};
-  this.user = {};
   this.loggedIn = false;
   this.formdata = {};
+  this.showOne = false;
+  this.showEdit = 0;
 
 
   // log in function
@@ -85,38 +89,21 @@ app.controller('MainController', ['$http', function($http) {
        });
      }
 
-     //DELETE LIST CATEGORY
-     this.delete = (id) => {
-       $http({
-         url: this.url + "/list_categories/" + id,
-         method: "DELETE"
-       }).then(response => {
-         this.list_categories.splice(response.data, 1);
-         this.getUser(this.oneUser_id);
-         this.getAllLists();
-       }).catch(reject => {
-         console.log('reject: ', reject);
-       });
-     }
+
 
      //CREATE LIST CATEGORY
-       this.createList = (list_id, user_id) => {
-         console.log("list id: " + list_id + " user id: " + user_id);
-         this.newCategory = {
-           user_id: user_id,
-           list_item_id: list_id
-         };
-         $http({
-           method: 'POST',
-           url: this.url + "/list_categories",
-           data: this.newCategory
-         }).then(response => {
-           this.list_categories.push(response.data)
-           this.getAllLists();
-         }).catch(error => {
-           console.log('error:', error);
-         });
-       }
+     this.createList_Item = () => {
+   $http({
+     method: 'POST',
+     url: this.url + "list_categories/" + this.list_category.id + '/list_items',
+     data: this.createForm
+   }).then((response) => {
+     console.log("New task:", response.data);
+     this.list_item = response.data;
+     this.list_items.unshift(this.list_item);
+   }).catch((err) => {
+     console.log("Error:", err);
+   })};
 
 
        //CREATE LIST ITEM
@@ -128,12 +115,45 @@ app.controller('MainController', ['$http', function($http) {
          }).then(response => {
            this.list = response.data;
            this.list_items.unshift(this.list);
-           this.createList(this.list.id, this.user.id)
+           this.createList(this.list_item.id, this.user.id)
            this.formdata = {}
-           console.log('this.list:', this.list);
+           console.log('this.list:', this.list_item);
          }).catch(error => {
            console.log('error:', error);
          });
        }
+
+      //EDIT LIST ITEM
+       this.editList_Item = (list_item) => {
+         this.list_item = list_item;
+         console.log(this.list_item);
+         console.log("Edit button works");
+         $http({
+           method: 'PUT',
+           url: this.url + "list_categories/" + this.list_category.id + '/list_items/' + this.list_item.id,
+           data: this.editForm
+         }).then((response) => {
+           console.log("Edited list_item:", response.data);
+           this.list_item = response.data;
+         }).catch((err) => {
+           console.log("Error:", err);
+         })};
+
+         this.showThisEdit = (list_item) => {
+           this.editForm = {};
+           this.showEdit = list_item.id;
+         };
+
+       //DELETE LIST CATEGORY
+       this.deleteList_Item = (list_itemToDelete) => {
+          $http({
+            method: 'DELETE',
+            url: this.url + "list_categories/" + this.list_category.id + '/list_items/' + list_itemToDelete.id
+          }).then((response) => {
+            const list_itemIndex = this.list_items.findIndex(list_item => this.list_item.id === list_itemToDelete.id);
+            this.list_items.splice(list_itemIndex, 1);
+          }).catch((err) => {
+            console.log("Error:", err);
+          })};
 
 }]);
