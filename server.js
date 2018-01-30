ddc79a3887d588a55f000515549fa11fbf5596a1
draconis79 <mandy.tong.79@gmail.com>
2018-01-29 16:23:35 -0800

    updated port

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app     = express();
-const PORT    = process.env.PORT || 3000;
+const PORT    = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+
+app.get('/*', function(req, res) {
+  res.sendfile('./public/index.html')
+})
 
 app.listen(PORT, () => console.log('everything list app - frontend: ', PORT));
